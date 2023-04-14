2cd6447c7ebcc7c17fa002ba47288b3820397e74
Kelly <long.redhair.tmge.106@gmail.com>
2023-04-14 19:37:49 +0900

    ヘッダ機能追加

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,63 @@
+<script setup lang="ts">
+import "../css/Calendar.css";
+import { computed, ref } from "vue";
+import CalendarHeader from "./CalendarHeader.vue";
+import { YearMonth } from "../../src/interfaces/YearMonth";
+
+const weekDays = ["日", "月", "火", "水", "木", "金", "土"];
+const currentDate = new Date();
+let yearMonth = ref<YearMonth>({
+  year: currentDate.getFullYear(),
+  month: currentDate.getMonth() + 1,
+});
+
+function currentYearMonth(currentyearMonth: YearMonth): void {
+  yearMonth.value = currentyearMonth;
+}
+console.debug("*** yearMonth: ", yearMonth);
+
+/**
+ * カレンダー本体
+ */
+let calendar: Array<Array<string>> = [];
+const computedCalendar = computed(() => {
+  let firstWeekDay = new Date(
+    yearMonth.value.year,
+    yearMonth.value.month - 1,
+    1
+  ).getDay();
+  let lastDay = new Date(
+    yearMonth.value.year,
+    yearMonth.value.month,
+    0
+  ).getDate();
+  let dayNumber = 1;
+
+  while (dayNumber < lastDay) {
+    let weekData: Array<string> = [];
+    for (let i = 0; i <= 6; i++) {
+      if (calendar.length == 0 && i < firstWeekDay) {
+        weekData[i] = "";
+      } else if (lastDay < dayNumber) {
+        weekData[i] = "";
+      } else {
+        weekData[i] = dayNumber.toString();
+        dayNumber++;
+      }
+    }
+    calendar.push(weekData);
+  }
+  return calendar;
+});
+calendar = computedCalendar.value;
+</script>
+
 <template>
   <div>
-    <CalendarHeader />
+    <CalendarHeader
+      v-model:YearMonth="yearMonth"
+      @emitYaerMonth="currentYearMonth"
+    />
     <table id="main">
       <thead>
         <th v-for="(weekDay, weekDayIndex) in weekDays" :key="weekDayIndex">
@@ -23,18 +80,3 @@
     </table>
   </div>
 </template>
-
-<script setup lang="ts">
-import "../css/Calendar.css";
-import CalendarHeader from "./CalendarHeader.vue";
-
-const weekDays = ["日", "月", "火", "水", "木", "金", "土"];
-const calendar = [
-  ["", "", "", "", "", "", 1],
-  [2, 3, 4, 5, 6, 7, 8],
-  [9, 10, 11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20, 21, 22],
-  [23, 24, 25, 26, 27, 28, 29],
-  [30, "", "", "", "", "", ""],
-];
-</script>
--- src/components/CalendarHeader.vue
+++ src/components/CalendarHeader.vue
@@ -1,48 +1,79 @@
-<template>
-  <div id="header">
-    <span class="header-arrow" @click="lastMonth">＜</span>
-    <span class="selected-month"
-      >{{ yearMonth.year }}年{{ yearMonth.month }}月</span
-    >
-    <span class="header-arrow" @click="nextMonth">＞</span>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { reactive } from "vue";
+import { defineEmits, ref } from "vue";
 import "../css/CalendarHeader.css";
+import { YearMonth } from "../../src/interfaces/YearMonth";
 
-interface YearMonth {
-  year: number;
-  month: number;
-}
-
-let yearMonth = reactive<YearMonth>({
-  year: 2023,
-  month: 4,
+const currentDate = new Date();
+let yearMonth = ref<YearMonth>({
+  year: currentDate.getFullYear(),
+  month: currentDate.getMonth() + 1,
 });
+console.debug("*** header_yearMonth: ", yearMonth);
+const emit = defineEmits(["emitYaerMonth"]);
 
 /**
- * 先月
+ * 現在年月
  */
-const lastMonth = () => {
-  yearMonth.month--;
-  if (yearMonth.month < 1) {
-    yearMonth.year--;
-    yearMonth.month = 12;
-  }
+const currentYearMonth = () => {
+  yearMonth.value.year = currentDate.getFullYear();
+  yearMonth.value.month = currentDate.getMonth() + 1;
+  emit("emitYaerMonth", yearMonth);
   return yearMonth;
 };
 
 /**
- * 次月
+ * 前年
+ */
+const prevYear = () => {
+  yearMonth.value.year--;
+  emit("emitYaerMonth", yearMonth);
+  return yearMonth;
+};
+
+/**
+ * 前月
+ */
+const prevMonth = () => {
+  yearMonth.value.month--;
+  if (yearMonth.value.month < 1) {
+    yearMonth.value.year--;
+    yearMonth.value.month = 12;
+  }
+  emit("emitYaerMonth", yearMonth);
+  return yearMonth;
+};
+
+/**
+ * 来月
  */
 const nextMonth = () => {
-  yearMonth.month++;
-  if (yearMonth.month > 12) {
-    yearMonth.year++;
-    yearMonth.month = 1;
+  yearMonth.value.month++;
+  if (yearMonth.value.month > 12) {
+    yearMonth.value.year++;
+    yearMonth.value.month = 1;
   }
+  emit("emitYaerMonth", yearMonth);
+  return yearMonth;
+};
+
+/**
+ * 来年
+ */
+const nextYear = () => {
+  yearMonth.value.year++;
+  emit("emitYaerMonth", yearMonth);
   return yearMonth;
 };
 </script>
+
+<template>
+  <div id="header">
+    <span class="header-arrow" @click="prevYear">◀</span>
+    <span class="header-arrow" @click="prevMonth">＜</span>
+    <span class="selected-month" @click="currentYearMonth"
+      >{{ yearMonth.year }}年{{ yearMonth.month }}月</span
+    >
+    <span class="header-arrow" @click="nextMonth">＞</span>
+    <span class="header-arrow" @click="nextYear">▶</span>
+  </div>
+</template>
