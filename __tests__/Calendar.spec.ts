24ad5c4ce083e56fe074fa1ab23e589a1b87d435
Kelly <long.redhair.tmge.106@gmail.com>
2023-04-17 17:32:39 +0900

    jest追加

--- __tests__/Calendar.spec.ts
+++ __tests__/Calendar.spec.ts
@@ -1,9 +1,16 @@
 import { render } from "@testing-library/vue";
+import { mount } from "@vue/test-utils";
 import Calendar from "../src/components/Calendar";
 
 describe("Calendar.vue", () => {
-  it("Calendar スナップショット(currentDateだけがNGの場合「yarn test:update」を実施)", () => {
-    const wrapper = render(Calendar);
+  const wrapper = render(Calendar);
+  it("Calendar スナップショット", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+  it("コンポーネントを描画できる", () => {
+    // Componentを描画できる
+    const wrapper = mount(Calendar);
+    const comp = wrapper.findComponent(Calendar);
+    expect(comp.exists()).toBe(true);
   });
 });
